f0731ce26a0b8483e632ecd4ee35b641cbba2e02
Gideon Shobowale <goshobowale@uclan.ac.uk>
2020-03-06 16:20:15 +0000

    Animation example

---
+++ moveshape.js
@@ -0,0 +1,110 @@
+window.addEventListener('load', eventWindowLoad, false);
+
+function eventWindowLoad() {
+    let c = document.getElementById("canvas");
+    let ctx = c.getContext("2d");
+    // Square
+    let x = 10; // x coordinates
+    let y = 10; // y coordinates
+    let width = 100; // Rectangle width
+    let height = 330; // Rectangle height
+
+    ///////////////////////////////////////
+    
+    // Animation logic
+    function logic() {
+        x += 0.1;
+        y += 0;
+        /*
+        if x + y is less than the width of canvas 
+        subtract the width if
+        answer = true draw and animate the shape
+        */
+        if (x + y < c.width - width) {
+            requestAnimationFrame(drawRec);
+        }
+
+
+        // Start animation loop
+        
+        for (let i = 0; i < 4; i++) {
+
+            if (x + y < c.width - width) {
+                requestAnimationFrame(drawRec);
+            }
+
+            // Stop the loop
+            else {
+                break;
+            }
+
+            console.log(x + y < c.width - width);
+        }
+
+    }
+
+    // Draw rectangle
+    function drawRec() {
+
+        let color = getRandomColor();
+        ctx.fillRect(x, y, width, height);
+        ctx.fillStyle = color;
+        ctx.fill();
+
+
+    }
+    // Execute animation
+    requestAnimationFrame(drawRec);
+    setInterval(logic, 2500 / 60); // delay/speed or frames
+    /////////////////////////////////
+    
+    // Show random colors
+    function getRandomColor() {
+        let characters = "0123456789ABCDEF";
+        let color = '#';
+        
+        // Loop colors
+        for (let i = 0; i < 6; i++) {
+            color += characters[getRandomNumber(20, 0)];
+        }
+
+        return color;
+    }
+    /*
+    Change color palette. High for (high saturation) and low for (low saturation).
+    */
+    function getRandomNumber(low, high) {
+        let r = Math.floor(Math.random() * (high - low + 2)) + low;
+        return r;
+    }
+
+};
+
+
+
+
+/* 
+
+else {
+        ctx.fillRect(10, 10, width, height);
+        ctx.fillStyle = '#';
+        ctx.fill();
+        }
+
+ function newLogic(){
+        x += 0;
+        y += 0;
+        
+        if (x + y < c.width - width ) {
+            requestAnimationFrame(drawRec);
+        }
+    
+    function drawRec() {
+        ctx.fillRect(x, y, width, height);
+        ctx.fill();
+    }
+    requestAnimationFrame(drawRec);
+    setInterval(newLogic, 2500 / 50); // delay/speed
+    
+    }
+*/
